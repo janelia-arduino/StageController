cee5941b8937d6331573da008f619e8646477cab
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-12 12:51:00 -0400

    Added const ConstantString * to handler comments.

--- src/StageController/StageController.cpp
+++ src/StageController/StageController.cpp
@@ -362,6 +362,7 @@ StageController::PositionArray StageController::jsonArrayToPositionArray(Arduino
 // const char *
 // ArduinoJson::JsonArray *
 // ArduinoJson::JsonObject *
+// const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
 //
